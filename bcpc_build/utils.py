1d8657725fd2e3535cb26cc6121b614b8e603937
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-12-15 06:27:01 -0500

    Rely upon system configuration for home directories.

--- bcpc_build/utils.py
+++ bcpc_build/utils.py
@@ -2,16 +2,15 @@ import shlex
 import os
 from subprocess import check_output
 
-HOME = '/var/lib'
-
 
 def useradd(username, *args, **kwargs):
     def user_homedir():
-        return os.path.join(HOME, username)
+        return os.path.join(kwargs['homedir_prefix'], username)
 
     kwargs = kwargs.copy()
-    def_args = '-m -r -U '
+    kwargs['args'] = '-m -r -U '
     kwargs['username'] = username
-    kwargs['args'] = def_args + '-d ' + user_homedir()
+    if 'homedir_prefix' in kwargs:
+        kwargs['args'] += ('-d ' + user_homedir())
     cmd = 'useradd {args} {username}'.format(**kwargs)
     return check_output(shlex.split(cmd))
