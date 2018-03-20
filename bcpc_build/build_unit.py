45f1c862b1042b155448e79506d4f5f24cb8be37
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2018-03-20 09:48:02 -0400

    Use detached HEADs for source checkouts
    Instead of assuming trees point to branches, use FETCH_HEAD to access revision
    specified in url.

--- bcpc_build/build_unit.py
+++ bcpc_build/build_unit.py
@@ -261,26 +261,46 @@ class BuildUnitAllocator(ABC):
                         password=creds['password'])
             args['url'] = url.url
             try:
-                path, branch = str(path).split('/tree/')
-                logger.debug('Detected branch %s from source url' % branch)
-                args['clone'] = '-b %s' % branch
-                # replace the path to exclude branch
+                path, rev = str(path).split('/tree/')
+                logger.debug('Detected revision %s from source url' % rev)
+                args['revision'] = rev
                 url.set(path=path)
                 args['url'] = url.url
             except (ValueError,) as e:
-                logger.debug(e)
+                logger.debug('Revision detection error: %s' % e)
             return args
 
-        def clone_cmd(src_url, dest=''):
-            args = git_args(src_url)
-            cmd = ("su -c 'git {git_args} clone {clone_args} {url} {dest}' "
-                   "{username}").format(git_args=args.get('git', ''),
-                                        clone_args=args.get('clone', ''),
-                                        url=args.get('url'),
-                                        dest=dest,
-                                        username=bunit.build_user)
-            logger.debug('Clone cmd `{}` from src_url={}'.format(cmd, src_url))
-            return cmd
+
+        def get_cmds(url, name):
+
+            def _checkout_cmd(rev, dest):
+                cmd = ("su -c"
+                    " 'git -C {dest} fetch origin {rev} &&"
+                    " git -C {dest} checkout FETCH_HEAD' "
+                    " {username}").format(rev=rev,
+                                          username=bunit.build_user,
+                                          dest=dest)
+                logger.debug('Checkout cmd `{}` from rev={}'.format(cmd, rev))
+                yield cmd
+
+            def _clone_cmd(src_url, name=''):
+                cmd = ("su -c 'git {git_args} clone {clone_args} {url} {name}' "
+                    "{username}").format(git_args=args.get('git', ''),
+                                         clone_args=args.get('clone', ''),
+                                         url=args.get('url'),
+                                         name=name,
+                                         username=bunit.build_user)
+                logger.debug('Clone cmd `{}` from src_url={}'.format(cmd, src_url))
+                yield cmd
+
+            cmds = []
+            args = git_args(url)
+            cmds += _clone_cmd(url, name)
+            rev = args.get('revision')
+            if rev:
+                dest = os.path.join(build_path, name)
+                cmds += _checkout_cmd(rev, dest)
+            return chain(cmds)
 
         def process_deps():
             for name in src_depends:
@@ -288,13 +308,13 @@ class BuildUnitAllocator(ABC):
                 # Suspect logger...
                 logger.debug('Processing dependency: {} => {}'
                              ''.format(name, url))
-                cmd = clone_cmd(url, name)
-                check_output(shlex.split(cmd))
+                for cmd in get_cmds(url, name):
+                    check_output(shlex.split(cmd))
 
         logger.info('Populating build unit...')
         process_deps()
-        cmd = clone_cmd(src_url, 'chef-bcpc')
-        check_output(shlex.split(cmd))
+        for cmd in get_cmds(src_url, 'chef-bcpc'):
+            check_output(shlex.split(cmd))
 
     def provision(self, build, *args, **kwargs):
         conf = kwargs.get('conf', {}).copy()
