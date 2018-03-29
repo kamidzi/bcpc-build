6877872cf547ead48310bb37450eff2c886506e5
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2018-03-29 11:24:34 -0400

    Add uniqueness, str representation to BuildStateEnum.

--- bcpc_build/db/migration_types.py
+++ bcpc_build/db/migration_types.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from enum import unique
 from sqlalchemy_utils.types.uuid import UUIDType
 
+@unique
 class BuildStateEnum(Enum):
     provisioned = 'provisioned'
     provisioning = 'provisioning'
@@ -11,3 +13,6 @@ class BuildStateEnum(Enum):
     failed = 'failed'
     failed_provision = 'failed:provision'
     failed_build = 'failed:build'
+
+    def __str__(self):
+        return self.value
