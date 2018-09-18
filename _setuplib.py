bfbb09691fc00d3cf0ca16a6e6df7d2d89791120
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2018-09-18 12:06:32 -0400

    Adds vboxapi installation
    vboxapi installation requires building from the VirtualBox SDK.

---
+++ _setuplib.py
@@ -0,0 +1,225 @@
+# https://gist.github.com/zeroslack/4175259ab36ab86d76ab08c88ddf37b4
+from glob import glob
+import contextlib
+import copy
+import importlib.util
+import os
+import os.path
+import platform
+import shlex
+import shutil
+import subprocess
+import tempfile
+import weakref
+import zipfile
+
+from lxml import html
+import certifi
+import requests
+
+try:
+    from contextlib import nullcontext
+except ImportError:
+
+    @contextlib.contextmanager
+    def nullcontext(enter_result=None):
+        if enter_result is None:
+            yield
+        else:
+            yield enter_result
+
+
+class SDKDownloader:
+    # N.B. - needs trailing slash in case of expansion with relative dir link
+    BASE_URL = 'https://download.virtualbox.org/virtualbox/'
+    sdk_regex = r".*SDK-{ver}.*\.zip$"
+
+    def __init__(self, base_url=BASE_URL):
+        self.base_url = base_url
+        self.cacert = None
+        # TODO: setup a partial to always include cert
+
+    def resolve_absolute_href(self, elem, base_url=BASE_URL):
+        elem = copy.copy(elem)
+        elem.make_links_absolute(base_url)
+        return elem.get('href')
+
+    def find_latest_stable_version(self, tree):
+        links = tree.xpath('//a[.="LATEST-STABLE.TXT"]')
+        assert len(links) == 1
+        elem = links.pop()
+        url_latest = self.resolve_absolute_href(elem)
+        return self.get(url_latest).text.strip()
+
+    # https://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+    @contextlib.contextmanager
+    def download_file(self, url, keep=True, **kwargs):
+        if keep:
+            cm = nullcontext(tempfile.mkdtemp())
+        else:
+            cm = tempfile.TemporaryDirectory()
+
+        with cm as tmpdir:
+            try:
+                local_filename = os.path.join(tmpdir, url.split('/')[-1])
+                r = self.get(url, stream=True)
+                try:
+                    with open(local_filename, 'wb') as f:
+                        shutil.copyfileobj(r.raw, f)
+                except OSError as e:
+                    raise RuntimeError(
+                        'Download of "{}" to {} failed'
+                        ''.format(url, local_filename)
+                    ) from e
+
+                yield os.path.abspath(local_filename)
+            finally:
+                pass
+
+    @staticmethod
+    @contextlib.contextmanager
+    def cacert_path(delete=True):
+        glob_patterns = ['*.crt', '*.pem']
+        kwargs_ = dict(delete=delete)
+        try:
+            ssl_cert_dir = os.environ.get('SSL_CERT_DIR', None)
+            if ssl_cert_dir is not None:
+
+                def _get_paths():
+                    for pat in glob_patterns:
+                        glob_ = os.path.join(ssl_cert_dir, pat)
+                        yield from glob(glob_)
+
+                with tempfile.NamedTemporaryFile('w', **kwargs_) as certfile:
+                    with open(certifi.where()) as existing_certs:
+                        certfile.write(existing_certs.read())
+
+                    for cert in _get_paths():
+                        try:
+                            with open(cert) as fh:
+                                certfile.write(fh.read())
+                        except OSError as e:
+                            raise RuntimeError(
+                                'Could not add certificate {}.'.format(cert)
+                            ) from e
+                    certfile.flush()
+                    yield certfile.name
+            else:
+                yield certifi.where()
+        finally:
+            pass
+
+    def get(self, url, **kwargs):
+        kwargs.setdefault('verify', self.cacert)
+        return requests.get(url, **kwargs)
+
+    @contextlib.contextmanager
+    def run(self, keep_files=False):
+        try:
+            if os.environ.get('SSL_CERT_DIR', None) is not None:
+                # TODO(kmidzi): need to cache
+                with self.__class__.cacert_path(delete=False) as path:
+                    self.cacert = path
+                    weakref.finalize(self, os.unlink, path)
+            else:
+                # This toggles based upon SSL_CERT_DIR
+                self.cacert = None
+
+            resp = self.get(self.base_url)
+            tree = html.fromstring(resp.content)
+            ver = self.find_latest_stable_version(tree)
+            dir_url = self.resolve_absolute_href(
+                tree.xpath('//a[.="{}/"]'.format(ver)).pop()
+            )
+            content = self.get(dir_url).content
+            tree = html.fromstring(content)
+
+            regex = self.sdk_regex.format(ver=ver)
+            sdk_elem = tree.xpath(
+                '//a[re:match(text(), "{regex}")]'.format(regex=regex),
+                namespaces={"re": "http://exslt.org/regular-expressions"},
+            ).pop()
+            sdk_url = self.resolve_absolute_href(sdk_elem, dir_url)
+            with self.download_file(sdk_url, keep=keep_files) as artifact:
+                yield artifact
+        finally:
+            pass
+
+
+if platform.system() == 'Linux':
+    _install_path = '/usr/lib/virtualbox'
+    _env = dict(VBOX_INSTALL_PATH=_install_path)
+else:
+    _env = {}
+
+
+@contextlib.contextmanager
+def env(**kwds):
+    oldenv = os.environ.copy()
+    try:
+        os.environ.update(kwds)
+        yield
+    finally:
+        os.environ = oldenv
+
+
+@contextlib.contextmanager
+def chdir(path):
+    try:
+        cwd = os.path.abspath(os.curdir)
+        os.chdir(path)
+        yield cwd
+    finally:
+        os.chdir(cwd)
+
+
+class VBoxAPIInstaller:
+    @staticmethod
+    def run(archive):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with chdir(tmpdir):
+                files = os.listdir(os.curdir)
+                fp = zipfile.ZipFile(archive)
+                fp.extractall()
+
+                newls = os.listdir(os.curdir)
+                newdir = (set(newls) - set(files)).pop()
+
+                def setup():
+                    # run the setup
+                    with chdir(os.path.join(newdir, 'installer')):
+                        setup_file = 'vboxapisetup.py'
+                        cmd = 'python {} install'.format(setup_file)
+                        with env(**_env):
+                            proc = subprocess.Popen(
+                                shlex.split(cmd), stdout=subprocess.PIPE
+                            )
+                            for line in iter(
+                                lambda: proc.stdout.readline(), b''
+                            ):
+                                print(line.decode('utf-8'), end='')
+                            ret = proc.wait()
+                        if ret != 0:
+                            raise RuntimeError('Installation failed!')
+
+                setup()
+
+    @staticmethod
+    def installation_test():
+        try:
+            _module = importlib.import_module('virtualbox')
+
+            def test_method():
+                vbox = _module.VirtualBox()
+                vbox.system_properties
+
+        except ImportError:
+
+            def test_method():
+                _module = importlib.import_module('vboxapi')
+                _module.VirtualBoxManager(None, None)
+
+        try:
+            test_method()
+        except Exception as e:
+            print('ERROR Using {}: {}'.format(_module.__file__, e))
--- setup.py
+++ setup.py
@@ -1,11 +1,73 @@
 from setuptools import setup, find_packages
+from distutils.errors import DistutilsSetupError
+
 # from setuptools.config import read_configuration
 # process setup.cfg - for uses like `pip install .`
 # Assumes python3
+import contextlib
+import pip
+import shlex
+import subprocess
+import sys
+import warnings
+
 try:
     import configparser
 except ImportError:
     import ConfigParser as configparser
+
+
+BUILD_TIME_DEPS = ('lxml', 'requests')
+
+
+# https://gist.github.com/zeroslack/daa9d85791079417d9217c330fdde146
+def _mk_opts(**kwargs):
+    def _opt_name(x):
+        return x.replace('_', '-')
+
+    def _process_kvs():
+        for k in kwargs:
+            optname = _opt_name(k)
+            flag_spec = '-' if len(optname) == 1 else '--'
+            if kwargs[k] is True:
+                yield '{}{}'.format(flag_spec, optname)
+            else:
+                yield '{}{} {}'.format(flag_spec, optname, kwargs[k])
+
+    return list(_process_kvs())
+
+
+def _mk_pip_fn(command):
+    def fn(name, global_opts={}, *args, **kwargs):
+        cmd = '{g_opts} {command} {args} {name}'.format(
+            g_opts=' '.join(_mk_opts(**global_opts)),
+            command=command,
+            args=' '.join(_mk_opts(**kwargs)),
+            name=name,
+        )
+        return pip.main((shlex.split(cmd)))
+
+    fn.__name__ = 'pip_{}'.format(command)
+    return fn
+
+
+pip_uninstall = _mk_pip_fn('uninstall')
+pip_install = _mk_pip_fn('install')
+
+
+@contextlib.contextmanager
+def buildtime_deps(*deps):
+    try:
+        for d in deps:
+            pip_install(d)
+        yield
+    finally:
+        for d in deps:
+            try:
+                pip_uninstall(d, yes=True)
+            except Exception:
+                warnings.warn('Failed to uninstall {}'.format(d))
+
 
 setup_args = {}
 try:
@@ -15,23 +77,24 @@ try:
     # config = read_configuration('setup.cfg')
     setup_args = dict(config['metadata'])
 except Exception as e:
-    import sys
     setup_args = {}
     print('Error reading setup.cfg: %s' % e, file=sys.stderr)
 
 # Update everything from setup.cfg
-setup_args.update(dict(
-    packages=find_packages(),
-    # TODO(kmidzi): offload
-    entry_points='''
+setup_args.update(
+    dict(
+        packages=find_packages(),
+        # TODO(kmidzi): offload
+        entry_points='''
         [console_scripts]
         bcpc-build=bcpc_build.cmd.main:cli
         bcpc-build-db=bcpc_build.cmd.db:cli
         bcpc-build-unit=bcpc_build.cmd.unit:cli
         bcpc-build-unit-config=bcpc_build.cmd.unit.config:cli
     ''',
-    use_scm_version = True
-))
+        use_scm_version=True,
+    )
+)
 
 
 def format_setup_args(conf):
@@ -47,3 +110,15 @@ def format_setup_args(conf):
 
 setup_args = format_setup_args(setup_args)
 setup(**setup_args)
+
+with buildtime_deps(*BUILD_TIME_DEPS):
+    import _setuplib
+
+    try:
+        with _setuplib.SDKDownloader().run() as filename:
+            _setuplib.VBoxAPIInstaller.run(filename)
+            _setuplib.VBoxAPIInstaller.installation_test()
+    except _setuplib.requests.exceptions.SSLError as e:
+        raise DistutilsSetupError(
+            'Try exporting SSL_CERT_DIR if you need additional CA certs.'
+        ) from e
