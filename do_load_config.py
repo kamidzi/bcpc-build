eaf593282e512116dff7023183c1dac8172804e9
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2019-06-22 06:47:43 -0400

    wip: load config.

--- do_load_config.py
+++ do_load_config.py
@@ -1,9 +1,15 @@
+from itertools import chain
 import logging
 import sys
-from itertools import chain
-
 
 from furl import furl
+from pprint import pprint
+import sqlalchemy as sa
+
+from bcpc_build.build_unit import V8BuildUnitAllocator
+from bcpc_build.build_unit import BuildUnit
+from bcpc_build.db import utils
+
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -12,88 +18,17 @@ bunit = lambda _: None
 bunit.build_user = 'build_user'
 
 
-def load_git_config():
-    return {
-        'https': {
-            'proxy': 'http://someproxy.com'
-        }
-    }
-
-build_path = '/fake/build/path'
-conf = load_git_config()
-
-def _load_creds(config):
-    if 'filename' in config:
-        # TODO(kamidzi): handle error and flesh out
-        with open(config['filename']) as fp:
-            raise NotImplementedError(
-                'Loading credentials from filename not supported.'
-            )
-
-
-# FIXME(kamidzi): git-credential helper?
-def git_args(url):
-    url = furl(url)
-    path = url.path
-    scheme = url.scheme
-    # Translate branch url to git arguments
-    args = {}
-    args['git'] = '-C %s' % build_path
-    if scheme in conf:
-        proxy = conf[scheme].get('proxy')
-        if proxy:
-            args['git'] += ' -c %s.proxy="%s"' % (scheme, proxy)
-
-    if 'credentials' in conf:
-        logger.debug('Detected credentials in configuration')
-        creds = conf['credentials']
-        url.set(username=creds['username'],
-                password=creds['password'])
-    args['url'] = url.url
-    try:
-        path, rev = str(path).split('/tree/')
-        logger.debug('Detected branch %s from source url' % rev)
-        args['revision'] = rev
-        url.set(path=path)
-        args['url'] = url.url
-    except (ValueError,) as e:
-        logger.debug('Revision/branch detection error: %s' % e)
-    return args
-
-
-def get_cmds(url, name):
-    def _checkout_cmd(rev, dest):
-        cmd = ("su -c"
-                " 'git -C {dest} fetch origin {rev} &&"
-                " git -C {dest} checkout FETCH_HEAD' "
-                " {username}").format(rev=rev,
-                                        username=bunit.build_user,
-                                        dest=dest)
-        logger.debug('Checkout cmd `{}` from rev={}'.format(cmd, rev))
-        yield cmd
-
-    def _clone_cmd(src_url, name=''):
-        cmd = ("su -c 'git {git_args} clone {clone_args} {url} {name}' "
-                "{username}").format(git_args=args.get('git', ''),
-                                    clone_args=args.get('clone', ''),
-                                    url=args.get('url'),
-                                    name=name,
-                                    username=bunit.build_user)
-        logger.debug('Clone cmd `{}` from src_url={}'.format(cmd, src_url))
-        yield cmd
-
-    cmds = []
-    args = git_args(url)
-    cmds += _clone_cmd(url, name)
-    rev = args.get('revision')
-    if rev:
-        dest = os.path.join(build_path, name)
-        cmds += _checkout_cmd(rev, dest)
-    return chain(cmds)
-
-
 if __name__ == '__main__':
-    bbgh = 'https://repo.example.com'
-    print(git_args(bbgh))
-    print(list(get_cmds(bbgh, 'chef-bcpc')))
+    session = utils.Session()
+    id_ = '48d00e2d-8832-40c7-8c13-acfba3b6e671'
+    id_ = '40b638b7-5a0a-47c6-99c4-732d296bb34f'
+    try:
+        # This will not have `logger` attribute
+        bunit = session.query(BuildUnit).get(id_)
+        import logging
+        bunit.logger = logging.getLogger(__name__)
+    except sa.exc.SQLAlchemyError as e:
+        sys.exit(e)
 
+    allocator = V8BuildUnitAllocator(session=session)
+    allocator.provision(bunit)
