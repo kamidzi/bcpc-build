313d922b40b12a53170a909cc1c09c34f5afd5dd
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2019-06-24 05:11:07 -0400

    Integrates impersonated_thread in test_nt_generator.py.

--- test_nt_generator.py
+++ test_nt_generator.py
@@ -9,7 +9,7 @@ import sys
 
 from bcpc_build.utils.vbox import get_vbox_sysprop
 from bcpc_build.utils import netid_from_name
-from bcpc_build.utils.credentials import env
+from bcpc_build.utils.credentials import impersonated_thread
 
 # contextlib.nullcontext only in python3.7+
 @contextlib.contextmanager
@@ -21,35 +21,16 @@ def nullcontext():
 
 
 if __name__ == '__main__':
-    # def generate_network_name(label)
-    #   @vmdir ||=  get_virtualbox_property('default_machine_folder')
-    #   mapping = {
-    #     label: label,
-    #     uid: Process.euid,
-    #     virtualbox_vm_dir: @vmdir,
-    #   }
-    #   @g ||= NetworkIDGenerator.new
-    #   id = @g.generate_id mapping
-    #   [label, id].join('-')
-    # end
-
-    new_uid = None
     try:
         username = sys.argv[1]
-        pw_ent = getpwnam(username)
-        new_uid = pw_ent.pw_uid
         sys.argv.pop(1)
     except Exception as e:
         sys.exit(e)
         
-    if new_uid:
-        setuid(new_uid)
-    #    setegid(new_uid)
-    #    seteuid(new_uid)
-    with env({'HOME': pw_ent.pw_dir}):
+    def do_work():
         vm_dir = get_vbox_sysprop('default_machine_folder')
+        print(vm_dir)
+        for label in sys.argv[1:]:
+            print(label, netid_from_name(label))
 
-    print(vm_dir)
-    for label in sys.argv[1:]:
-        print(label, netid_from_name(label))
-
+    impersonated_thread(username, do_work)
